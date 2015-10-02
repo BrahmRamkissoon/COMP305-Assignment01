09d0b04d4311b6646ae588edb417f2fa544c0328
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2015-10-02 00:48:44 -0400

    Added scrolling background, updated external documentation

---
+++ Assets/_Scripts/BGScroller.cs
@@ -0,0 +1,32 @@
+﻿// Source file name: BGScroller.cs
+// Author: Brahm Ramkissoon
+// 
+// Last Modified by: Brahm Ramkissoon
+// Date created: 01/10/2015
+// Date last Modified: 
+// File description: 
+// Revision history:
+using UnityEngine;
+using System.Collections;
+
+public class BGScroller : MonoBehaviour
+{
+    public float ScrollSpeed;       // Set scroll speed of quad
+    public float TileSizeZ;         // Size of tile
+
+    private Vector3 _startPosition;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _startPosition = GetComponent<Transform>().position;    // get position of quad
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    float newPosition = Mathf.Repeat(Time.time * ScrollSpeed, TileSizeZ);     // never exceed length of tile 
+        transform.position = _startPosition + Vector3.forward * newPosition;    // reset quad position
+	
+	}
+}
