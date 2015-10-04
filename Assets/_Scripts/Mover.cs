6ac1c148ad7edac4f484f09c5abac08319d398d4
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2015-10-04 12:37:58 -0400

    Added laser bolts prefab. updated external doc

---
+++ Assets/_Scripts/Mover.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+
+    public float speed;         // control speed of object
+
+    // move automatically on game start
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = transform.forward * speed;
+    }
+}
--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,15 +1,56 @@
-﻿using UnityEngine;
+﻿// Source file name: PlayerController.cs
+// Author: Brahm Ramkissoon
+// 
+// Last Modified by: Brahm Ramkissoon
+// Date created: 04/10/2015
+// Date last Modified: 
+// File description: 
+// Revision history:
+
+using UnityEngine;
 using System.Collections;
+
+// Define boundaries of visible play area
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;    
+}
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public float speed;         // Set the movement speed of player object
+    public Boundary boundary;   // Create instance of Boundary
+    public float xTilt;         // tilt player object on z axis
+    public float zTilt;         // tilt player object on y axis
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        // increase player object movement speed
+        GetComponent<Rigidbody>().velocity = movement * speed;
+
+        // prevent player object from leaving visible area
+        GetComponent<Rigidbody>().position = new Vector3
+        (
+            Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+        );
+
+        // simulate forward and sideways physics with tilt along z and x axes
+        // based on player velocity along x and z axes respectively
+        GetComponent<Rigidbody>().rotation = Quaternion.Euler
+            (
+                GetComponent<Rigidbody>().velocity.z * zTilt, 
+                0.0f, 
+                GetComponent<Rigidbody>().velocity.x * -xTilt
+            );
+
+
+    }
 }
