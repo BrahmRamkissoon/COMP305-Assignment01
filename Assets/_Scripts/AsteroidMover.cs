f6c62d6b8c0153b5ce7ced9305f39e4a3bcad913
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2015-10-04 21:40:32 -0400

    Imported Vast Outer Space Resource, Added Asteroid Prefab, tweaked random speed Added player and asteroid explosions Added AsteroidMover script Tweaked DestroyByContact script to ignore asteroid vs asteroid contact

---
+++ Assets/_Scripts/AsteroidMover.cs
@@ -0,0 +1,23 @@
+﻿// Source file name: AsteroidMover.cs
+// Author: Brahm Ramkissoon
+// 
+// Last Modified by: Brahm Ramkissoon
+// Date created: 04/10/2015
+// Date last Modified: 
+// File description: Randomize asteroid movement along Z axis
+// Revision history:
+
+using UnityEngine;
+using System.Collections;
+
+public class AsteroidMover : MonoBehaviour
+{
+
+    // move automatically on game start
+    void Start()
+    {
+        // Set asteroid movement speed randomly along the z axis between -4.0 and -7.0
+        GetComponent<Rigidbody>().velocity = transform.forward * Random.Range(-4.0F, -7.0F);
+        
+    }
+}
---
+++ Assets/_Scripts/DestroyByContact.cs
@@ -0,0 +1,48 @@
+﻿// Source file name: DestroyByContact.cs
+// Author: Brahm Ramkissoon
+// 
+// Last Modified by: Brahm Ramkissoon
+// Date created: 04/10/2015
+// Date last Modified: 
+// File description: Object collisions should have explosions, exceptions below:
+//      Asteroid to Asteroid
+//      Object to Boundary 
+// Revision history:    -   all collisions produce explosions except with Boundary
+//                      -   added explosion exception: asteroid to asteroid collisions
+
+using UnityEngine;
+using System.Collections;
+
+// Destroy any objects which collide with each other
+public class DestroyByContact : MonoBehaviour
+{
+
+    public GameObject explosion;            // Explosion effect for object
+    public GameObject playerExplosion;      // Explosion effect for player object explosion
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Ignore boundary collision
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+        // Create explosion at object position, rotation
+        Instantiate(explosion, transform.position, transform.rotation);
+
+        if (other.tag == "Asteroid")
+        {
+            // need to add code for physcis
+            return;
+        }
+
+        // Create explosion at player object position, rotation
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+        }
+        
+        Destroy(other.gameObject);      
+        Destroy(gameObject);
+        }
+}
